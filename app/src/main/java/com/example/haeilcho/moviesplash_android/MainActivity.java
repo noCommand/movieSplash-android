d6b8aa42dee4cbcb033ed2b9f19a4b8ef27fe329
Haeil Cho <ncommend@naver.com>
2017-06-30 16:35:31 +0900

    Initial commit

---
+++ app/src/main/java/com/example/haeilcho/moviesplash_android/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.haeilcho.moviesplash_android;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
